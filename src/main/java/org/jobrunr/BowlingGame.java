638518f9f2538a867f039fc9b3c2dfdfb939beee
Shango Bashi <7328674+shangobashi@users.noreply.github.com>
2026-01-08 19:51:27 +0100

    My solution
    Implemented scoring logic for bowling game rolls, including handling strikes and spares.

--- src/main/java/org/jobrunr/BowlingGame.java
+++ src/main/java/org/jobrunr/BowlingGame.java
@@ -2,11 +2,43 @@ package org.jobrunr;
 
 public class BowlingGame {
 
+    private int score = 0;
+    private int rollsInFrame = 0;
+    private int frame = 1;
+    private int previousRoll = 0;
+    private boolean spare = false;
+    private boolean strike = false;
+
     public void roll(int pinsDown) {
-        throw new UnsupportedOperationException();
+
+        // bonus handling
+        if (strike) {
+            score += pinsDown * 2;
+            strike = false;
+        } else if (spare) {
+            score += pinsDown;
+            spare = false;
+        }
+
+        score += pinsDown;
+        rollsInFrame++;
+
+        if (pinsDown == 10) {
+            strike = true;
+            frame++;
+            rollsInFrame = 0;
+        } else if (rollsInFrame == 2) {
+            if (previousRoll + pinsDown == 10) {
+                spare = true;
+            }
+            frame++;
+            rollsInFrame = 0;
+        }
+
+        previousRoll = pinsDown;
     }
 
     public int score() {
-        throw new UnsupportedOperationException();
+        return score;
     }
 }
